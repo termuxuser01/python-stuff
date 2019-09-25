2ef47aa32841ea08e953eeceeb52bf27ad2a411b
termuxuser01 <53441033+termuxuser01@users.noreply.github.com>
2019-09-25 17:36:24 -0500

    Create upper.py

---
+++ challenges/upper.py
@@ -0,0 +1,6 @@
+# Write a program that accepts sequence of 
+# lines as input and prints the lines after making all characters in the sentence capitalized.
+
+str1 = input("enter a sentence:\n")
+
+print(str1.upper())
