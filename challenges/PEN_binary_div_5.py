c56afb87a823b92ced14ed7b3a5268eb8ab88d55
termuxuser01 <53441033+termuxuser01@users.noreply.github.com>
2019-09-27 17:33:22 -0500

    Update PEN_binary_div_5.py
    finally finished 100% working ftw!!!

--- challenges/PEN_binary_div_5.py
+++ challenges/PEN_binary_div_5.py
@@ -2,30 +2,24 @@
 # and then check whether they are divisible by 5 or not. 
 # The numbers that are divisible by 5 are to be printed in a comma separated sequence.
 
+def val_check(bin_nums):
+  for i in bin_nums.split(","):
+    for n in i:
+      if n != "1" and n != "0":
+        print("BINARY!!!")
+        bin_acc()
+    if len(i) > 4:
+      print("FOUR DIGITS!!!")
+      bin_acc()
+    elif int(i, 2) % 5 == 0:
+      print(i, end=",")
+
+
 def bin_acc():
-    global bin_nums
     bin_nums = input("write a list of four digit binary numbers:\n")
+    val_check(bin_nums)
 
 bin_acc()
-while True:
-    try:
-        for i in int(bin_nums.split(",")):
-            for n in i:
-                if int(n) != 1 or int(n) != 0:
-                    print("BINARY!!!")
-                    bin_acc()
-            if len(i) > 4:
-                print("FOUR DIGITS!!!")
-                bin_acc()
-            elif int(i) % 5 == 0:
-                print(i, end=",")
-            else:
-                continue
-    except ValueError:
-        print("HMMMMM...")
-        bin_acc()
-    except TypeError:
-        print("HMMMMM...")
-        bin_acc()
 
-    break
+
+
