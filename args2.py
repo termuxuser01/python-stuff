0e5a2ad153e4d2e04395108fd8b1ee1cac7c0584
termuxuser01 <53441033+termuxuser01@users.noreply.github.com>
2020-05-30 18:50:43 -0400

    added logging support

--- args2.py
+++ args2.py
@@ -3,20 +3,32 @@ import argparse
 import configparser
 # store resault in file
 import sys
+# use of cron to schedule execution
+from datetime import datetime
+# enable error loggin
+import logging
+
+LOG_FORMAT = '%(asctime)s %(name)s %(levelname)s %(message)s'
+LOG_LEVEL = logging.DEBUG
 
 def main(num1, num2, output):
     r = num1 * num2
-    print('the resault is {}'.format(r), file=output)
+    print('[{}] the resault is {}'.format(datetime.utcnow().strftime('%d/%m/%Y %X'), r), file=output)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-n1', type=int, default=0, help='One of the multiplicants')
     parser.add_argument('-n2', type=int, default=0, help='The other multiplicant')
     # add the optional config file to the program
-    parser.add_argument('-c', '--config', type=argparse.FileType('r'), help='a config file')
+    parser.add_argument('-c', '--config', type=argparse.FileType('r'), help='a config file',
+                        default='/etc/automate.ini')
     # configure the flag to output to a file
-    parser.add_argument('-o', type=argparse.FileType('w'), help='Output file',
+    # append to log
+    parser.add_argument('-o', type=argparse.FileType('a'), help='Output file',
                         dest='output', default=sys.stdout)
+    # add option for logging file
+    parser.add_argument('-l', dest='log', type=str, help='log file',
+                        default=None)
 
     args = parser.parse_args()
 
@@ -33,4 +45,13 @@ if __name__ == '__main__':
         args.n1 = int(config['ARGUMENTS']['n1'])
         args.n2 = int(config['ARGUMENTS']['n2'])
 
-    main(args.n1, args.n2, args.output)
+    if args.log:
+        logging.basicConfig(format=LOG_FORMAT, level=LOG_LEVEL, filename=args.log)
+    else:
+        logging.basicConfig(format=LOG_FORMAT, level=LOG_LEVEL)
+
+    try:
+        main(args.n1, args.n2, args.output)
+    except Exception as exp:
+        logging.exception('There was an error, exiting...')
+        exit(1)
